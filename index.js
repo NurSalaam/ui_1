6c0b89729e568de82a156ff45014cc1c48acff49
Nur Mogammad Salaam <36481471+NurSalaam@users.noreply.github.com>
2023-11-25 14:35:04 +0200

    Fix weird input field zooming

--- index.js
+++ index.js
@@ -7,3 +7,27 @@ document.querySelector(".btn").addEventListener("click", () => {
 document.querySelector("a").addEventListener("click", () => {
   document.querySelectorAll("#role").remove();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var inputFields = document.querySelectorAll("input, textarea");
+
+  inputFields.forEach(function (input) {
+    input.addEventListener("focus", function () {
+      disableZoom();
+    });
+
+    input.addEventListener("blur", function () {
+      enableZoom();
+    });
+  });
+
+  function disableZoom() {
+    document.documentElement.style.overflow = "hidden";
+    document.body.scroll = "no"; // For some older versions of iOS
+  }
+
+  function enableZoom() {
+    document.documentElement.style.overflow = "auto";
+    document.body.scroll = "yes"; // For some older versions of iOS
+  }
+});
